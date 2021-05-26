bd6c377e11b634d1baed3dc38d2437b28c2c9579
Reza sohrbi <rezasohrabi1995@yahoo.com>
2021-05-27 00:41:09 +0430

    Reformat codes with prettier

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,41 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.scss'
+import Head from 'next/head';
+import styles from '../styles/Home.module.scss';
 
 export default function Home() {
   return (
-    <div className="App">
+    <div className='App'>
       <Head>
         <meta charSet='utf-8' />
         <title>Reza Sohrabi | Resume Website</title>
-        <meta name="description" content="Reza Sohrabi is a front-end developer (react)" />
+        <meta
+          name='description'
+          content='Reza Sohrabi is a front-end developer (react)'
+        />
         <meta property='og:type' content='resume website' />
-        <meta name='og:title' property='og:title' content='Reza Sohrabi | Resume Website' />
-        <meta name='og:description' property='og:description' content='Reza Sohrabi is a front-end developer (react)' />
-        <meta name='og:url' property='og:url' content='http://www.rezasohrabi.com' />
+        <meta
+          name='og:title'
+          property='og:title'
+          content='Reza Sohrabi | Resume Website'
+        />
+        <meta
+          name='og:description'
+          property='og:description'
+          content='Reza Sohrabi is a front-end developer (react)'
+        />
+        <meta
+          name='og:url'
+          property='og:url'
+          content='http://www.rezasohrabi.com'
+        />
         <meta name='og:image' property='og:image' content='/favicon.png' />
         <meta name='viewport' content='width=device-width, initial-scale=1' />
         <meta name='twitter:title' content='Reza Sohrabi | Resume Website' />
-        <meta name='twitter:description' content='Reza Sohrabi is a front-end developer (react)' />
-        <link rel="icon" href="/favicon.png" />
+        <meta
+          name='twitter:description'
+          content='Reza Sohrabi is a front-end developer (react)'
+        />
+        <link rel='icon' href='/favicon.png' />
       </Head>
     </div>
-  )
+  );
 }
