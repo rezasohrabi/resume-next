27dab10c2a735d69dcf65a975f07b05f504d5854
Reza sohrbi <rezasohrabi1995@yahoo.com>
2021-05-27 12:50:01 +0430

    Add LifeStory component

---
+++ components/LifeStory/index.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const LifeStory = ({ story }) => {
+  return (
+    <section className='stotry'>
+      <div className='story__date'>{story.time}</div>
+      <a className='story__link' href={story.fromLink} target='_blank'>
+        <img className='story__image' src={story.image} />
+      </a>
+      <div className='story__info'>
+        <div className='header'>{story.title}</div>
+        <div>
+          At :{''}
+          <a className='from-link' href={story.fromLink} target='_blank'>
+            {story.from}
+          </a>
+        </div>
+        <div className='desc'>{story.description}</div>
+      </div>
+    </section>
+  );
+};
