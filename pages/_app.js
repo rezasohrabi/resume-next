549b8942991d89cc8db6af3f79508625ab9a58a4
rezasohrabi <rezasohrabi1995@yahoo.com>
2022-11-23 18:32:48 +0330

    feature: add microsoft clarity

--- pages/_app.js
+++ pages/_app.js
@@ -18,7 +18,27 @@ function App({ Component, pageProps }) {
       })(window, document, 'https://static.hotjar.com/c/hotjar-', '.js?sv=');
     }
   }
+
+  function initializeClarity() {
+    if (typeof window !== 'undefined') {
+      (function (c, l, a, r, i, t, y) {
+        c[a] =
+          c[a] ||
+          function () {
+            (c[a].q = c[a].q || []).push(arguments);
+          };
+        t = l.createElement(r);
+        t.async = 1;
+        t.src = 'https://www.clarity.ms/tag/' + i;
+        y = l.getElementsByTagName(r)[0];
+        y.parentNode.insertBefore(t, y);
+      })(window, document, 'clarity', 'script', 'enfdrs352s');
+    }
+  }
+
   initializeHotjar();
+  initializeClarity();
+
   return <Component {...pageProps} />;
 }
 
