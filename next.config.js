3175ff170df9d0ccbde78449f545df23870dbfae
rezasohrabi <rezasohrabi1995@yahoo.com>
2023-02-15 21:54:11 +0330

    feat: add pwa support to app

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true',
-});
+const withPWA = require('next-pwa');
 
-module.exports = withBundleAnalyzer({});
+module.exports = withPWA({
+  reactStrictMode: true,
+  pwa: {
+    dest: 'public',
+    register: true,
+    skipWaiting: true,
+  },
+});
--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import '../styles/globals.scss';
 
 function App({ Component, pageProps }) {
@@ -39,7 +40,34 @@ function App({ Component, pageProps }) {
   initializeHotjar();
   initializeClarity();
 
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <meta charSet='utf-8' />
+        <meta httpEquiv='X-UA-Compatible' content='IE=edge' />
+        <meta
+          name='viewport'
+          content='width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no'
+        />
+        <link rel='manifest' href='/manifest.json' />
+        <link
+          href='/favicon-16x16.png'
+          rel='icon'
+          type='image/png'
+          sizes='16x16'
+        />
+        <link
+          href='/favicon-32x32.png'
+          rel='icon'
+          type='image/png'
+          sizes='32x32'
+        />
+        <link rel='apple-touch-icon' href='/apple-touch-icon.png'></link>
+        <meta name='theme-color' content='#26a69a' />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default App;
