97143fddfe7194b9812a85c868b2d9f5f00516e9
r.sohrabi <rezasohrabi1995@yahoo.com>
2023-11-14 09:44:17 +0330

    add gtm

--- pages/_app.js
+++ pages/_app.js
@@ -37,8 +37,24 @@ function App({ Component, pageProps }) {
     }
   }
 
+  function initializeGTM() {
+    if (typeof window !== 'undefined') {
+      (function (w, d, s, l, i) {
+        w[l] = w[l] || [];
+        w[l].push({ 'gtm.start': new Date().getTime(), event: 'gtm.js' });
+        var f = d.getElementsByTagName(s)[0],
+          j = d.createElement(s),
+          dl = l != 'dataLayer' ? '&l=' + l : '';
+        j.async = true;
+        j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+        f.parentNode.insertBefore(j, f);
+      })(window, document, 'script', 'dataLayer', 'GTM-PWN6DKR2');
+    }
+  }
+
   initializeHotjar();
   initializeClarity();
+  initializeGTM();
 
   return (
     <>
@@ -64,6 +80,14 @@ function App({ Component, pageProps }) {
         />
         <link rel='apple-touch-icon' href='/apple-touch-icon.png'></link>
         <meta name='theme-color' content='#26a69a' />
+        <noscript>
+          <iframe
+            src='https://www.googletagmanager.com/ns.html?id=GTM-PWN6DKR2'
+            height='0'
+            width='0'
+            style='display:none;visibility:hidden'
+          ></iframe>
+        </noscript>
       </Head>
       <Component {...pageProps} />
     </>
